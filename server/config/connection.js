0dbd36bbda9c4fcd2ead2958d2121068dfd3df29
Nova <eldanadesalgne@gmail.com>
2022-02-10 12:27:54 -0500

    updated files to run

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book_Search_Engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
