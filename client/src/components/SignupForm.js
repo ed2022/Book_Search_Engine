f8ca487a350fb1c3419bf351a246c773fbc6dbda
Nova <eldanadesalgne@gmail.com>
2022-02-16 13:39:09 -0500

    creating new branch

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -34,12 +34,6 @@ const SignupForm = () => {
 
     try {
       const { data } = await addUser({variables: { ...userFormData }});
-
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
-      // const { token, user } = await response.json();
-      // console.log(user);
       Auth.login(data.token);
     } catch (err) {
       console.error(err);
